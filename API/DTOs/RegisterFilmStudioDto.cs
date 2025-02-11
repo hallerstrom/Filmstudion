414dd4ad310a082d01809b2128b39f2f4a6a55db
Linus Hallerström <hallerstrom.linus@gmail.com>
2025-02-11 19:24:11 +0100

    Added controller and more

--- API/DTOs/RegisterFilmStudioDto.cs
+++ API/DTOs/RegisterFilmStudioDto.cs
@@ -7,5 +7,7 @@ public class RegisterFilmStudioDto :IRegisterFilmStudio
 {
     public string Name { get; set; }
     public string City { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
 
 }
